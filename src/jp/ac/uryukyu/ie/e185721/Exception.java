bb3ab96aaa346e1172bf8436403366e3f39a440e
Kaito Arakaki <e185721@ie.u-ryukyu.ac.jp>
2018-11-13 13:37:56 +0900

    report4 code

--- src/jp/ac/uryukyu/ie/e185721/Exception.java
+++ src/jp/ac/uryukyu/ie/e185721/Exception.java
@@ -2,8 +2,14 @@ package jp.ac.uryukyu.ie.e185721;
 
 public class Exception {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }catch (NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
+
     }
 
 }
