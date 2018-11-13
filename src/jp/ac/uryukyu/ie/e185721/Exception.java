8df31bd1fc03af344589509b439b6180e63100d8
Kaito Arakaki <e185721@ie.u-ryukyu.ac.jp>
2018-11-13 13:22:32 +0900

    report4 code

---
+++ src/jp/ac/uryukyu/ie/e185721/Exception.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.ie.e185721;
+
+public class Exception {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.length());
+    }
+
+}
